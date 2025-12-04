9002ae78366c1b6b97f5b32a121a8c4a5ab15471
TizianaNapoli <napolitizy@gmail.com>
2025-12-04 17:16:49 +0100

    Aggiunte funzionalità a quizzettone: navigazione tra domande, correzione numero risposte statistiche

--- lezioni/week02/quizzettone/main.py
+++ lezioni/week02/quizzettone/main.py
@@ -15,10 +15,11 @@ def estrai_domanda(content : str, index : int) -> str:
 def estrai_risposta(content : str, index : int) -> str:
     return content[index+1:]
     
-def mostra_domanda(domanda : str) -> None:
+def mostra_domanda(domanda : str, domanda_attuale : int, domande_totali : int) -> None:
     """
     Questa funzione restituisce la domanda e le optioni di risposta
     """
+    print(f"Domanda {domanda_attuale} di {domande_totali}")
     print(domanda)
     
 def raccogli_risposta() -> str:
@@ -94,6 +95,65 @@ def genera_statistiche(risultato_finale : list[dict[str, str |bool ]]) -> dict[s
 
     return statistica
 
+def naviga_tra_le_domande(contatore_domanda_attuale : int, domande_totali : int) -> int :
+
+    navigazione : bool = True
+
+    navigazione_prima_domanda : bool = False
+
+    while navigazione :
+
+        scelta_utente : str = input("<-- P (Domanda precedente)  ***  (Domanda successiva) S --> ")
+
+        if scelta_utente.upper() == "P" :
+
+            if contatore_domanda_attuale == 0 :
+
+                print("")
+                print("*"*30)
+                print("Nessuna domanda precedente!")
+                print("*"*30)
+                print("")
+
+                navigazione_prima_domanda = True
+
+                while navigazione_prima_domanda :
+
+                    scelta_utente = input("R (Ripeti prima domanda)  ***  (Domanda successiva) S --> ")
+
+                    if scelta_utente.upper() == "S" :
+                        contatore_domanda_attuale += 1
+                        return contatore_domanda_attuale
+
+                    elif scelta_utente.upper() == "R" :
+                        contatore_domanda_attuale == 0
+                        return contatore_domanda_attuale
+
+                    else:
+                        print("") 
+                        print("Inserisci solo R o S")
+                        print("")
+                               
+            else :
+                contatore_domanda_attuale -= 1
+                return contatore_domanda_attuale
+            
+        elif scelta_utente.upper() == "S" :
+
+            if contatore_domanda_attuale < domande_totali :
+                contatore_domanda_attuale += 1
+                if contatore_domanda_attuale == domande_totali :
+                    print("")
+                    print("*"*30)
+                    print("Domande terminate!")
+                    print("*"*30)
+            return contatore_domanda_attuale
+        
+        else :
+            print("")
+            print("Inserisci solo P o S")
+            print("")
+
 def main()  :
 
     lista_domande: list[str] = []
@@ -108,13 +168,14 @@ def main()  :
     is_risposta_corretta = False
     
     while counter_domanda_corrente < lista_domande_lenght :
+        
         risultato : dict[str, str | bool] = {}
         content : str = leggi_file(F"domande_risposte/{lista_domande[counter_domanda_corrente]}")
         index : int = estrai_index(content)
         domanda_e_risposta["domanda"] = estrai_domanda(content, index)
         domanda_e_risposta["risposta"] = estrai_risposta(content, index)
 
-        mostra_domanda(domanda_e_risposta["domanda"])
+        mostra_domanda(domanda_e_risposta["domanda"], counter_domanda_corrente+1, lista_domande_lenght)
         risposta_da_validare : str = raccogli_risposta()
         risposta_validata : bool = valida_scelta(risposta_da_validare)
 
@@ -123,15 +184,22 @@ def main()  :
             feedback = genera_feedback(is_risposta_corretta)
             risultato["domanda"] = lista_domande[counter_domanda_corrente]
             risultato["risposta_corretta"] = is_risposta_corretta
-            risultato_finale.append(risultato)
-            counter_domanda_corrente += 1
+
+            if counter_domanda_corrente < len(risultato_finale) :
+                risultato_finale[counter_domanda_corrente] = risultato
+            else:
+                risultato_finale.append(risultato)
+
+            mostra_feedback(feedback)
+            counter_domanda_corrente = naviga_tra_le_domande(counter_domanda_corrente, lista_domande_lenght)  
         else:
             feedback = "Inserisci solo la risposta tra le optioni elencate"
-        
-        mostra_feedback(feedback)
-
+            mostra_feedback(feedback)
+           
     statistiche = genera_statistiche(risultato_finale)
 
+    print("")
+    print("--- Il tuo punteggio ---")
     print(f"Risposte corrette: {statistiche["risposte_esatte"]}")
     print(f"Risposte sbagliate: {statistiche["risposte_errate"]}")    
     
