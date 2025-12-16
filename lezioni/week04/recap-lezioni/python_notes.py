310265eb14f028840ae4d2eabde5eb115b41c480
TizianaNapoli <napolitizy@gmail.com>
2025-12-16 09:46:41 +0100

    week 04: ripasso

---
+++ lezioni/week04/recap-lezioni/python_notes.py
@@ -0,0 +1,19 @@
+"""
+input -> programma -> output
+
+Tipi di input:
+- file nel file system
+    - open("nomefile", "r")
+- input da parte dell'utente
+- dati che vengono da altri computer sulla rete
+
+Tipi di output:
+- file nel file system
+    - open("nomefile", "w")
+- output UI
+- dati che vengono mandati ad altri computer sulla rete
+
+Programma == Set di istruzioni
+
+Compilatore traduce il nostro codice in linguaggio macchina
+"""
---
+++ lezioni/week04/recap-lezioni/supermercato.py
@@ -0,0 +1,17 @@
+"""
+Obiettivo:
+
+Creare un programma che simuli una spesa al supermercato. Il programma deve gestire una Lista della Spesa (ciò che devi comprare) e un Carrello (ciò che stai prendendo).
+
+Scenario:
+
+Siamo nel 2050. I carrelli del supermercato sono robotizzati. Per evitare sprechi, il carrello si rifiuta di aprirsi se cerchi di inserire un prodotto che non è nella tua lista della spesa digitale.
+
+Cosa fare:
+
+-Analisi e definizione di tutto ciò che serve al nostro programma per funzionare e per terminare
+
+-Disegno del flusso (draw.io)
+
+-Implementazione del codice
+"""
